137f95a9b370d8a4c4cbf0f9838ae6ba8e2066a5
Vasily Bodnarchuk <vasilybodnarchuk@gmail.com>
2020-08-18 11:22:54 -0400

    updated tests for `ValueProcessingSerialQueue`

--- Example/Tests/ValueProcessingSerialQueueSpec.swift
+++ Example/Tests/ValueProcessingSerialQueueSpec.swift
@@ -18,15 +18,27 @@ class ValueProcessingSerialQueueSpec: QuickSpec {
             var serialQueue: ValueProcessingSerialQueue!
             beforeEach { serialQueue = ValueProcessingSerialQueue() }
             
-            it("expects append") {
+            it("first waits for all closures to be added and then executes them in the correct order") {
                 print(serialQueue)
                 var array1 = [Int]()
                 var array2 = [Int]()
-                for _ in 0..<10 {
+                for _ in 0..<10_000 {
                     array1.append(array1.count)
                     serialQueue.append { array2.append(array2.count) }
                 }
                 serialQueue.perform()
+                expect(array1) == array2
+            }
+            
+            it("adds closures to the queue and immediately executes them in the correct order") {
+                print(serialQueue)
+                var array1 = [Int]()
+                var array2 = [Int]()
+                for _ in 0..<10_000 {
+                    array1.append(array1.count)
+                    serialQueue.append { array2.append(array2.count) }
+                    serialQueue.perform()
+                }
                 expect(array1) == array2
             }
         }
